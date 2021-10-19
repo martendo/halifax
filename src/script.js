678f6534cc62ea04af30945ef783faaaf7aaee12
martendo <51766598+martendo@users.noreply.github.com>
2021-10-19 19:24:47 -0400

    Improve lerping

--- src/script.js
+++ src/script.js
@@ -10,8 +10,8 @@ try {
 
 if (hasWebGL) {
 	const LERP_DONE_DISTANCE = 0.001;
-	const SELECT_LERP = [new THREE.Vector3(1.4, 1.4, 1.4), 0.15];
-	const DESELECT_LERP = [new THREE.Vector3(1, 1, 1), 0.15];
+	const SELECT_SCALE = new THREE.Vector3(1.4, 1.4, 1.4);
+	const DESELECT_SCALE = new THREE.Vector3(1, 1, 1);
 
 	let lerps = [];
 
@@ -23,25 +23,56 @@ if (hasWebGL) {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 
-	const hemiLight = new THREE.HemisphereLight(0xfffff0, 0x606060, 0.5);
-	scene.add(hemiLight);
-	const dirLight = new THREE.DirectionalLight(0xffff0e0, 0.5);
-	dirLight.position.set(1, 1, 2);
-	scene.add(dirLight);
-
 	const geometry = new THREE.IcosahedronGeometry();
 	const material = new THREE.MeshLambertMaterial({
-		color: 0xff0000,
+		color: 0xa0a0a0,
 		polygonOffset: true,
 		polygonOffsetFactor: 1,
 		polygonOffsetUnits: 1,
 	});
 	const die = new THREE.Mesh(geometry, material);
+	die.enterFunc = function() {
+		lerps = lerps.filter((lerp) => lerp.uuid !== die.uuid && lerp.uuid !== dirLight.uuid);
+		// Brighten spotlight and enlarge die
+		lerps.push({
+			vector: dirLight,
+			prop: "intensity",
+			target: 0.8,
+			alpha: 0.25,
+			uuid: dirLight.uuid,
+		}, {
+			vector: die.scale,
+			target: SELECT_SCALE,
+			alpha: 0.15,
+			uuid: die.uuid,
+		});
+	};
+	die.leaveFunc = function() {
+		lerps = lerps.filter((lerp) => lerp.uuid !== die.uuid && lerp.uuid !== dirLight.uuid);
+		// Dim spotlight and shrink die
+		lerps.push({
+			vector: dirLight,
+			prop: "intensity",
+			target: 0.5,
+			alpha: 0.25,
+			uuid: dirLight.uuid,
+		}, {
+			vector: die.scale,
+			target: DESELECT_SCALE,
+			alpha: 0.15,
+			uuid: die.uuid,
+		});
+	};
 	scene.add(die);
 	const edges = new THREE.EdgesGeometry(geometry);
 	const line = new THREE.LineSegments(edges);
 	line.material.color.setHex(0x000000);
 	die.add(line);
+
+	const hemiLight = new THREE.HemisphereLight(0xfffff0, 0x606060, 0.5);
+	const dirLight = new THREE.DirectionalLight(0xffff0e0, 0.5);
+	dirLight.position.set(1, 1, 2);
+	scene.add(hemiLight, dirLight);
 
 	camera.position.z = 3;
 
@@ -73,39 +104,17 @@ if (hasWebGL) {
 			const newIntObj = intersects[0].object;
 			if (intObj != newIntObj) {
 				// Intersecting a new object
-				if (intObj) {
-					// Restore intersect object's colour
-					intObj.material.color.setHex(intObj.prevColor);
+				if (intObj && intObj.leaveFunc) {
+					intObj.leaveFunc();
 				}
-				if (newIntObj.material.emissive) {
-					// Colour the new object
-					intObj = newIntObj;
-					intObj.prevColor = intObj.material.color.getHex();
-					intObj.material.color.setHex(0xff4020);
-					// Lerp scale
-					lerps = lerps.filter((lerp) => lerp.uuid !== intObj.uuid);
-					const lerp = {
-						vector: intObj.scale,
-						args: SELECT_LERP,
-						uuid: intObj.uuid,
-					};
-					intObj.lerp = lerp;
-					lerps.push(lerp);
+				intObj = newIntObj;
+				if (intObj.enterFunc) {
+					intObj.enterFunc();
 				}
 			}
 		} else {
-			if (intObj) {
-				// Restore intersect object's colour
-				intObj.material.color.setHex(intObj.prevColor);
-				// Lerp scale
-				lerps = lerps.filter((lerp) => lerp.uuid !== intObj.uuid);
-				const lerp = {
-					vector: intObj.scale,
-					args: DESELECT_LERP,
-					uuid: intObj.uuid,
-				};
-				intObj.lerp = lerp;
-				lerps.push(lerp);
+			if (intObj && intObj.leaveFunc) {
+				intObj.leaveFunc();
 			}
 			// Not intersecting any objects
 			intObj = null;
@@ -126,11 +135,29 @@ if (hasWebGL) {
 	function updateLerps() {
 		for (let i = lerps.length - 1; i >= 0; i--) {
 			const lerp = lerps[i];
-			lerp.vector.lerp(...lerp.args);
-			if (lerp.vector.distanceTo(lerp.args[0]) < LERP_DONE_DISTANCE) {
-				// Lerp is pretty much finished -> remove it
-				lerps.splice(i, 1);
-				lerp.vector.copy(lerp.args[0]);
+			let vector = lerp.vector;
+			if (lerp.prop) {
+				vector = vector[lerp.prop];
+			}
+			if (typeof vector == "number") {
+				vector += (lerp.target - vector) * lerp.alpha;
+				if (Math.abs(lerp.target - vector) < LERP_DONE_DISTANCE) {
+					// Lerp is pretty much finished -> remove it
+					lerps.splice(i, 1);
+					vector = lerp.target;
+				}
+				if (lerp.prop) {
+					lerp.vector[lerp.prop] = vector;
+				} else {
+					lerp.vector = vector;
+				}
+			} else {
+				vector.lerp(lerp.target, lerp.alpha);
+				if (vector.distanceTo(lerp.target) < LERP_DONE_DISTANCE) {
+					// Lerp is pretty much finished -> remove it
+					lerps.splice(i, 1);
+					vector.copy(lerp.target);
+				}
 			}
 		}
 	}
